cc73165e6f9ba2557aec5259c0dfd1ab8975dd8f
Etishagarg <gargetisha@gmail.com>
2024-08-24 16:30:40 +0530

    Initial Commit

---
+++ Shadow-in-SwiftUI/Shadow_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shadow_in_SwiftUIApp.swift
+//  Shadow-in-SwiftUI
+//
+//  Created by Etisha Garg on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Shadow_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
